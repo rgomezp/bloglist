ff80c5488be6c3487cc680026a40036ff0801280
rgomezp <rogomezpalacio@gmail.com>
2016-07-29 17:34:49 -0500

    initial commit

---
+++ js/timer.js
@@ -0,0 +1,21 @@
+$(function(){
+  function timer(){
+    seconds = seconds-1;
+    if (seconds == 0){
+      clearTimeout(timeout);
+    }
+    update(seconds)
+  }
+
+  function update(seconds){
+    var minutes = Math.floor(seconds / 60);
+    var seconds2 = seconds % 60;
+    if(seconds2 < 10){
+      seconds2 = "0"+seconds2;
+    }
+    console.log(seconds);
+    $("#timer").html(minutes + ":" + seconds2);
+  }
+  var seconds = 600;
+  var timeout = setInterval(timer,1000);
+})
